ad20e319c0089220b1ae35b91ac67be3c4eb1042
wanru0215 <113331789+wanru0215@users.noreply.github.com>
2025-10-08 16:25:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -240,6 +240,11 @@ app.get('/api/log/conversation/:studentId', async (req, res) => {
   }
 });
 
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // 靜態檔案伺服
 app.use(express.static(path.join(__dirname, "public")));
 
